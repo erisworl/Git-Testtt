639db8f5898c4c6d1ab4ad260429e064a7424860
Erik Worley <erisworl@ut.utm.edu>
2018-11-28 00:45:26 -0600

    Fourth Commit: adding updated main.cpp.

--- main.cpp
+++ main.cpp
@@ -3,10 +3,26 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-cout << "Hello, world!" << endl;
+void Sum(int);
 
-cin.ignore();
-cin.get();
-return 0;
+int main() {
+	int n;
+	int sum;
+
+	cout << "Enter a number for n:";
+	cin >> n;
+
+	Sum(n);
+
+	cin.ignore();
+	cin.get();
+	return 0;
+}
+
+void Sum(int n) {
+	int sum = 0;
+	for (int i = 0; i <= n; i++) {
+		sum += i;
+	}
+	cout << "The sum is " << sum << "." << endl;
 }
